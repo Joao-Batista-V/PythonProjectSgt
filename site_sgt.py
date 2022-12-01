ff7af7c5b8337af886d50134d3114edbf9723c42
joao.batistas <joao.asta83@gmail.com>
2022-11-30 22:55:13 -0300

    Conexão com o banco de dados sqlite

--- site_sgt.py
+++ site_sgt.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
 
 """
 - Flask: responsável pela conexão da aplicação com o servidor
@@ -7,6 +8,21 @@ from flask import Flask, render_template, url_for
 """
 
 app = Flask(__name__)  # Aplicativo Flask
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_contatos.sqlite3'
+
+db = SQLAlchemy(app)
+
+# Banco de dados db_contato
+class tb_contato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    mensagem = db.Column(db.String(200))
+    def __init__(self, nome, email, mensagem):
+        self.nome = nome
+        self.email = email
+        self.mensagem = mensagem
 
 @app.route('/sobre')
 def sobre():
@@ -35,9 +51,21 @@ def midia():
     """
     return render_template("midia.html")
 
-@app.route('/contato')
+@app.route('/contato', methods=["GET", "POST"])
 def contato():  # Neste módulo será implementado um sistema de formulários
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    mensagem = request.form.get('mensagem')
+
+    if request.method == 'POST':
+        contato = tb_contato(nome, email, mensagem)
+        db.session.add(contato)
+        db.session.commit()
     return render_template("contato.html")
+
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":  # Bloco de execusão do código principal. A função debug retornará eventuais erros
     app.run(debug=True)
